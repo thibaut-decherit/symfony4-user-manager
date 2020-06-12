be044754b87399734f348d310c3cb686acac3d29
Thibaut Decherit <tdecherit@entreloups.com>
2020-06-12 09:45:48 +0200

    test: update

--- tests/Controller/User/SecurityControllerTest.php
+++ tests/Controller/User/SecurityControllerTest.php
@@ -27,14 +27,11 @@ class SecurityControllerTest extends AbstractWebTest
                 ->getValue();
 
         $client->followRedirects();
-        $crawler = $client->request('GET', '/logout', [
+        $client->request('GET', '/logout', [
             '_csrf_token' => $csrfToken
         ]);
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertEquals(
-            'Hi, anonymous visitor', $crawler->filter('.container > p')->first()->text()
-        );
 
         $tokenClass = get_class($client->getContainer()->get('security.token_storage')->getToken());
         $this->assertEquals(AnonymousToken::class, $tokenClass);
